bb17b2a69ef051bf175593568247a5fa1b600d7c
khaoscomplex <goodamon@aol.com>
2016-11-12 20:28:52 -0500

    Gun Works!

---
+++ Assets/FireGun.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireGun : MonoBehaviour {
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float fireRate = 0.5f;
+    private float nextFire = 0.5f;
+
+    void Update()
+    {
+
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
