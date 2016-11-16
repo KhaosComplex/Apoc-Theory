ce15739b4b51113372464a65c7f5158acb31b499
Derek Z <jundraptor@gmail.com>
2016-11-16 00:28:05 -0500

    stuff

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,7 +12,9 @@ public class PlayerController : MonoBehaviour
     private float gravityRate = 1f;
 
     private bool isOnGround;
+    private bool hitStun;
     private CharacterController controller;
+    private float hitStunTime = 0.2f;
 
     void Start()
     {
@@ -22,46 +24,58 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        //GET THE CHARACTER CONTROLLER AND DIRECTION OF MOVEMENT IN RELATION TO SPEED
-        float horizontalMovement = Input.GetAxis("Horizontal");
-        float verticalMovement = Input.GetAxis("Vertical");
-
-        //CREATE THE MOVEMENT DIRECTION VECTOR
-        Vector3 moveDirection = new Vector3(horizontalMovement, 0, verticalMovement);
-        moveDirection *= movementSpeed;
-        moveDirection = transform.TransformDirection(moveDirection);
-
-        //IF SHIFT KEY OR RIGHT CLICK IS PRESSED
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetMouseButtonDown(1))
+        if (hitStun)
         {
-            // GET THE CHANGE IN DISTANCE REQUIRED TO DASH (AKA DIRECTION)
-            float deltaX = horizontalMovement;
-            float deltaZ = verticalMovement;
-
-            // AS LONG AS BOTH DISTANCES AREN'T 0
-            if (!(deltaX == 0 && deltaZ == 0))
+            hitStunTime -= Time.deltaTime;
+            if (hitStunTime <= 0.0f)
             {
-                //GET THE HYPOTENUSE OF THE TWO DISTANCES
-                float hyp = Mathf.Pow(Mathf.Pow(deltaX, 2f) + Mathf.Pow(deltaZ, 2f), .5f);
-                //FIGURE OUT THE APPROPIATE RATE TO APPLY TO DELTAX/Y TO TRAVEL THE DASH DISTANCE IN THE PROPER DIRECTION
-                float rate = dashDistance / hyp;
-
-                //FINALLY, SET THE POSITION
-                transform.position = new Vector3(transform.position.x + (deltaX * rate), transform.position.y, transform.position.z + (deltaZ * rate));
+                hitStun = false;
+                hitStunTime = 0.2f;
             }
         }
-
-        //IF PLAYER IS ON THE GROUND AND SPACE IS PRESSED
-        if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        else
         {
-            gravityRate = jumpSpeed;
+            //GET THE CHARACTER CONTROLLER AND DIRECTION OF MOVEMENT IN RELATION TO SPEED
+            float horizontalMovement = Input.GetAxis("Horizontal");
+            float verticalMovement = Input.GetAxis("Vertical");
+
+            //CREATE THE MOVEMENT DIRECTION VECTOR
+            Vector3 moveDirection = new Vector3(horizontalMovement, 0, verticalMovement);
+            moveDirection *= movementSpeed;
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            //IF SHIFT KEY OR RIGHT CLICK IS PRESSED
+            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetMouseButtonDown(1))
+            {
+                // GET THE CHANGE IN DISTANCE REQUIRED TO DASH (AKA DIRECTION)
+                float deltaX = horizontalMovement;
+                float deltaZ = verticalMovement;
+
+                // AS LONG AS BOTH DISTANCES AREN'T 0
+                if (!(deltaX == 0 && deltaZ == 0))
+                {
+                    //GET THE HYPOTENUSE OF THE TWO DISTANCES
+                    float hyp = Mathf.Pow(Mathf.Pow(deltaX, 2f) + Mathf.Pow(deltaZ, 2f), .5f);
+                    //FIGURE OUT THE APPROPIATE RATE TO APPLY TO DELTAX/Y TO TRAVEL THE DASH DISTANCE IN THE PROPER DIRECTION
+                    float rate = dashDistance / hyp;
+
+                    //FINALLY, SET THE POSITION
+                    transform.position = new Vector3(transform.position.x + (deltaX * rate), transform.position.y, transform.position.z + (deltaZ * rate));
+                }
+            }
+
+            //IF PLAYER IS ON THE GROUND AND SPACE IS PRESSED
+            if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                gravityRate = jumpSpeed;
+            }
+
+            //****BUG WITH isGrounded***** APPLY A DOWNWARD FORCE IF ON THE GROUND TO KEEP isGrounded TRUE
+            if (controller.isGrounded) moveDirection.y = gravityRate;
+
+            //MOVE THE PLAYER
+            controller.Move(moveDirection * Time.deltaTime);
         }
-
-        //****BUG WITH isGrounded***** APPLY A DOWNWARD FORCE IF ON THE GROUND TO KEEP isGrounded TRUE
-        if (controller.isGrounded) moveDirection.y = gravityRate;
-
-        //MOVE THE PLAYER
-        controller.Move(moveDirection * Time.deltaTime);
 
     }
 
@@ -97,6 +111,10 @@ public class PlayerController : MonoBehaviour
 
     public void takeDamage(float damage)
     {
-        HP = HP - damage;
+        if (hitStun == false)
+        {
+            HP = HP - damage;
+            hitStun = true;
+        }
     }
 }
