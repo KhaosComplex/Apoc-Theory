aface548ac41677c99c9db2410d29ff2788a4a90
Derek Z <jundraptor@gmail.com>
2016-11-18 18:26:23 -0500

    shock

---
+++ Assets/Scripts/MeleeController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeleeController : MonoBehaviour {
+
+    private bool damageOK = false;
+
+    //UPDATES IF BOSS IS IN MELEE HITBOX
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Boss"))
+            damageOK = true;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Boss"))
+            damageOK = false;
+    }
+
+    //IS BOSS IN MELEE HITBOX
+    public bool doesDamage()
+    {
+        return damageOK;
+    }
+}
---
+++ Assets/Scripts/ShockController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShockController : MonoBehaviour {
+    [SerializeField] private Transform end;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float stretchSpeed;
+    [SerializeField] private float waitTime;
+	
+	// Update is called once per frame
+	void Update () {
+        if (waitTime > 0)
+            waitTime -= Time.fixedDeltaTime;
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, end.position, moveSpeed * Time.deltaTime);
+
+            transform.localScale = new Vector3(Mathf.MoveTowards(transform.localScale.x, end.localScale.x, stretchSpeed * Time.deltaTime),
+                Mathf.MoveTowards(transform.localScale.y, end.localScale.y, stretchSpeed * Time.deltaTime),
+                Mathf.MoveTowards(transform.localScale.z, end.localScale.z, stretchSpeed * Time.deltaTime));
+        }
+        if(transform.position == end.position)
+        {
+            Destroy(gameObject.transform.parent.gameObject);
+        }
+	}
+}
