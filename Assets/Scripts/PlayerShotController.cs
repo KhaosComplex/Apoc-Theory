b6d98bd4ccc78321787dd1f7de58a20aca3c968a
Alexander Greenstein <goodamon@aol.com>
2016-11-29 15:08:13 -0500

    Fixed OBSS bug

--- Assets/Scripts/PlayerShotController.cs
+++ Assets/Scripts/PlayerShotController.cs
@@ -7,17 +7,12 @@ public class PlayerShotController : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float damage;
     private Rigidbody rb;
-    private GameObject bossObject;
-    private GameObject obeliskBossShotObject;
 
     void Start()
     {
         //WHEN PROJECTILE IS SPAWNED, SEND IT FORWARD
         rb = GetComponent<Rigidbody>();
         rb.velocity = transform.forward * speed;
-
-        bossObject = GameObject.FindWithTag("Boss");
-        obeliskBossShotObject = GameObject.FindWithTag("ObeliskBossShot");
     }
 
     void OnTriggerEnter(Collider other)
@@ -25,14 +20,14 @@ public class PlayerShotController : MonoBehaviour
         //IF BOSS GETS HIT, HAVE BOSS LOSE HEALTH
         if (other.gameObject.tag.Equals("Boss"))
         {
-            BossController bossController = bossObject.GetComponent<BossController>();
+            BossController bossController = other.gameObject.GetComponent<BossController>();
             if (!bossController.isImmune()) bossController.takeDamage(damage);
 
             Destroy(this.gameObject);
         }
         else if (other.gameObject.tag.Equals("ObeliskBossShot"))
         {
-            obeliskBossShotObject.GetComponent<ObeliskBossShotController>().takeDamage(damage);
+            other.gameObject.GetComponent<ObeliskBossShotController>().takeDamage(damage);
             Destroy(this.gameObject);
         }
     }
