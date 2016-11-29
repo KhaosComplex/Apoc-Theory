aee6b5ec7b216968e1bf675d10d8dd150a2f4c03
Alexander Greenstein <goodamon@aol.com>
2016-11-28 23:34:52 -0500

    Added dash shockwave. Random number generator choses between 3 potential ones

--- Assets/BossShockwaveController.cs
+++ Assets/BossShockwaveController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class BossShockwaveController : MonoBehaviour
 {
     [SerializeField] private GameObject shockwave;
+    [SerializeField] private GameObject shockwaveUP;
     [SerializeField] private float timeToSpawnShockwave;
     [SerializeField] private float timeBetweenShockwaveSpawn;
     private int whichShockwaveToSpawn;
@@ -13,11 +14,24 @@ public class BossShockwaveController : MonoBehaviour
         if (Time.timeSinceLevelLoad >= timeToSpawnShockwave)
         {
             GameObject shockwaveInstance;
+            whichShockwaveToSpawn = Random.Range(0, 3);
             if (whichShockwaveToSpawn == 0)
             {
                 shockwaveInstance = (GameObject)Instantiate(shockwave, new Vector3(0, 0, -42.77f), shockwave.transform.rotation);
                 timeToSpawnShockwave = Time.timeSinceLevelLoad + timeBetweenShockwaveSpawn;
             }
+            else if (whichShockwaveToSpawn == 1)
+            {
+                shockwaveInstance = (GameObject)Instantiate(shockwaveUP, new Vector3(0, 0, -42.77f), shockwaveUP.transform.rotation);
+                timeToSpawnShockwave = Time.timeSinceLevelLoad + timeBetweenShockwaveSpawn;
+            }
+            else
+            {
+                shockwaveInstance = (GameObject)Instantiate(shockwave, new Vector3(0, 0, -42.77f), shockwave.transform.rotation);
+                shockwaveInstance = (GameObject)Instantiate(shockwaveUP, new Vector3(0, 0, -42.77f), shockwaveUP.transform.rotation);
+                timeToSpawnShockwave = Time.timeSinceLevelLoad + timeBetweenShockwaveSpawn;
+            }
+            Debug.Log(whichShockwaveToSpawn);
         }
     }
 }
