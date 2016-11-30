076238389ee9feb87169a63a73bce8ab4bdf4140
Alexander Greenstein <goodamon@aol.com>
2016-11-30 02:05:31 -0500

    Moving to desktop

--- Assets/Scripts/BossShooter.cs
+++ Assets/Scripts/BossShooter.cs
@@ -12,28 +12,33 @@ public class BossShooter : MonoBehaviour
     [SerializeField] private float fireRate = 0.5f;
     private float nextFire = 0.5f;
 
+    [SerializeField] private float timeToStart;
+
     [SerializeField] private bool movingRight;
 
     void Update()
     {
-        //CONTINUE ROTATING GUN IN THE DIRECTION IT'S TRAVELING
-        if (movingRight == true)
-            controller.rotation = Quaternion.RotateTowards(controller.rotation, end.rotation, Time.deltaTime * speed);
-       else
-            controller.rotation = Quaternion.RotateTowards(controller.rotation, start.rotation,Time.deltaTime * speed);
-
-        //IF GUN HAS ROTATED FAR ENOUGH, BEGIN ROTATING IN OTHER DIRECTION
-        if (movingRight == true && controller.eulerAngles.y <= end.eulerAngles.y)
-            movingRight = false;
-        else if (movingRight == false && controller.eulerAngles.y >= start.eulerAngles.y)
-            movingRight = true;
-
-        //NOW MAKE SURE THE GUN FIRES CONSISTENT WITH THE FIRE RATE
-        if (Time.timeSinceLevelLoad > nextFire)
+        if (Time.timeSinceLevelLoad >= timeToStart)
         {
-            nextFire = Time.timeSinceLevelLoad + fireRate;
-            GameObject shotHolder = (GameObject)Instantiate(shot, controller.position, controller.rotation);
-            shotHolder.transform.parent = GameObject.Find("Boss Shots").transform;
+            //CONTINUE ROTATING GUN IN THE DIRECTION IT'S TRAVELING
+            if (movingRight == true)
+                controller.rotation = Quaternion.RotateTowards(controller.rotation, end.rotation, Time.deltaTime * speed);
+            else
+                controller.rotation = Quaternion.RotateTowards(controller.rotation, start.rotation, Time.deltaTime * speed);
+
+            //IF GUN HAS ROTATED FAR ENOUGH, BEGIN ROTATING IN OTHER DIRECTION
+            if (movingRight == true && controller.eulerAngles.y <= end.eulerAngles.y)
+                movingRight = false;
+            else if (movingRight == false && controller.eulerAngles.y >= start.eulerAngles.y)
+                movingRight = true;
+
+            //NOW MAKE SURE THE GUN FIRES CONSISTENT WITH THE FIRE RATE
+            if (Time.timeSinceLevelLoad > nextFire)
+            {
+                nextFire = Time.timeSinceLevelLoad + fireRate;
+                GameObject shotHolder = (GameObject)Instantiate(shot, controller.position, controller.rotation);
+                shotHolder.transform.parent = GameObject.Find("Boss Shots").transform;
+            }
         }
     }
 }
